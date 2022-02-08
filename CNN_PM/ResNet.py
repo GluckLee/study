f0640a202b330576f49e202a1bf60a5952e846b6
glucklee <lijixiang2020@gmail.com>
2022-02-08 20:24:44 +0800

    ResNet_show

--- CNN_PM/ResNet.py
+++ CNN_PM/ResNet.py
@@ -23,12 +23,16 @@ class ConvBNLayer(paddle.nn.Layer):
         self._batch_norm = paddle.nn.BatchNorm2D(num_filters)
         self.act = act
     def forward(self, inputs):
+        print('ConvBNLayer***********************************************:')
         y = self._conv(inputs)
+        print(y.shape)
         y = self._batch_norm(y)
         if self.act == 'leaky':
             y = F.leaky_relu(x=y, negative_slope=0.1)
         elif self.act == 'relu':
             y = F.relu(x=y)
+        print(y.shape)
+        print('ConvBNLayer***********************************************:')
         return y
 
 class BottleneckBlock(paddle.nn.Layer):
@@ -64,15 +68,23 @@ class BottleneckBlock(paddle.nn.Layer):
         self._num_channels_out = num_filters*4
 
     def forward(self, inputs):
+        print('BottleneckBlock###################################################:')
         y = self.conv0(inputs)
+        print(y.shape)
         conv1 = self.conv1(y)
+        print(conv1.shape)
         conv2 = self.conv2(conv1)
+        print(conv2.shape)
         if self.shortcut:
             short = inputs
         else:
             short = self.short(inputs)
+        print('--------->', short.shape, conv2.shape)
         y = paddle.add(x=short, y=conv2)
+        print('--------->', y.shape)
         y = F.relu(y)
+        print(y.shape)
+        print('BottleneckBlock###################################################:')
         return y
 
 class ResNet(paddle.nn.Layer):
@@ -104,9 +116,8 @@ class ResNet(paddle.nn.Layer):
         # 遍历c2, c3, c4, c5
         for block in range(len(depth)):     # block:0，1，2，3
             shortcut = False
-            # i 是【0,1,2】【0,1,2,3】【0,1,2,3,4,5】【0,1,2】
-            for i in range(depth[block]):  # depth:3, 4, 6, 3
-                print('bb_%d_%d' % (block, i))
+            # depth:[3, 4, 6, 3]
+            for i in range(depth[block]):  # i 是【0,1,2】【0,1,2,3】【0,1,2,3,4,5】【0,1,2】
                 bottleneck_block = self.add_sublayer(
                     'bb_%d_%d' % (block, i),
                     BottleneckBlock(
@@ -125,22 +136,22 @@ class ResNet(paddle.nn.Layer):
                                  initializer=paddle.nn.initializer.Uniform(-stdv, stdv)))
 
     def forward(self, inputs):
-        print('0', inputs.shape)
+        print('ResNet:')
+        print('in', inputs.shape)
         y = self.conv(inputs)
-        print('1', y.shape)
+        print(y.shape)
         y = self.pool2d_max(y)
-        print('2', y.shape)
+        print('c1', y.shape)
         # bottleneck_block表示c2,c3,c4,c5
         for i, bottleneck_block in enumerate(self.bottleneck_block_list):
-            print(i, bottleneck_block)
             y = bottleneck_block(y)
-            print('------------------------------------------------------------------3-{}'.format(i), y.shape)
+            # print('c2-c5{}'.format(i), y.shape)
         y = self.pool2d_avg(y)
-        print('4', y.shape)
+        print('pool_avg', y.shape)
         y = paddle.reshape(y, [y.shape[0], -1])
-        print(y.shape)
+        print('reshape', y.shape)
         y = self.out(y)
-        print(y.shape)
+        print('out', y.shape)
         time.sleep(9999)
         return y
 
